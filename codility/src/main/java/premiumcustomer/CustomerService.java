f7514cc1668ce6d77ec19f4465554e75c6b88290
anna.rysicz <anna.rysicz@assecods.pl>
2020-07-14 15:28:56 +0200

    premium cusomer method update

--- codility/src/main/java/premiumcustomer/CustomerService.java
+++ codility/src/main/java/premiumcustomer/CustomerService.java
@@ -9,9 +9,9 @@ public class CustomerService {
     private CustomerRepository customerRepository;
 
     public void updateCustomerToPremiumStatus(Customer customer) {
-
-        customer.setStatus(customer.getStatus().equals(Status.FRAUD) ? Status.FRAUD : Status.PREMIUM);
-
-        customerRepository.save(customer);
+        if (!customer.getStatus().equals(Status.FRAUD)) {
+            customer.setStatus(Status.PREMIUM);
+            customerRepository.save(customer);
+        }
     }
 }

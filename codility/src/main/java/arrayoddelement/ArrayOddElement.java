eab3013b00596182a1fe43b964fca6ae48a54fd9
Anna Rysicz <anna@rysicz.pl>
2020-07-28 14:44:20 +0200

    arrayOddElemment task added

---
+++ codility/src/main/java/arrayoddelement/ArrayOddElement.java
@@ -0,0 +1,22 @@
+package arrayoddelement;
+
+import java.util.HashSet;
+
+//Do not use any lists if using contains a lot - kills the performance
+public class ArrayOddElement {
+    public int solution(int[] A) {
+        return getOddElement(A);
+    }
+
+    public int getOddElement(int[] A) {
+        HashSet<Integer> a = new HashSet<>();
+        for (int i=0; i < A.length; i++) {
+            if (a.contains(A[i])) {
+                a.remove(A[i]);
+            } else {
+                a.add(A[i]);
+            }
+        }
+        return a.stream().findFirst().get();
+    }
+}

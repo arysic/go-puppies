2a6f73e0a0e0fe01fd9c82948415d792330d688b
Anna Rysicz <anna@rysicz.pl>
2020-08-04 09:25:50 +0200

    counters new solution task aded

--- codility/src/main/java/counters/Counters.java
+++ codility/src/main/java/counters/Counters.java
@@ -1,9 +1,43 @@
 package counters;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Counters {
     public int[] solution(int N, int[] A) {
+        int max = 0;
+        int maxChange = 0;
+        int tmp;
+        Map<Integer, Integer> changeMap = new HashMap<>();
+        for (int i = 0; i < A.length; i++) {
+            if(A[i] > N) {
+                max += maxChange;
+                changeMap.clear();
+                maxChange = 0;
+            } else {
+                tmp = (changeMap.get(A[i]) == null ? 0 : changeMap.get(A[i])) + 1;
+                changeMap.put(A[i], tmp);
+                maxChange = tmp > maxChange ? tmp : maxChange;
+            }
+        }
+        return computeResult(max, changeMap, N);
+    }
+
+    private int[] computeResult(int max, Map<Integer, Integer> changeMap, int N) {
+        int[] result = new int[N];
+        int change;
+        for(int i = 0; i < N; i++) {
+            change = changeMap.get(i + 1) == null ? 0 : changeMap.get(i + 1);
+            result[i] = max + change;
+        }
+        return result;
+    }
+
+    private int getMaxOld(Map<Integer, Integer> changeMap) {
+        Map.Entry<Integer, Integer> maxEntry = Collections.max(changeMap.entrySet(), Comparator.comparing(Map.Entry::getValue));
+        return maxEntry.getValue();
+    }
+
+    public int[] solutionOld(int N, int[] A) {
         int[] result = new int[N];
         int max = 0;
         for (int i = 0; i < A.length; i++) {
--- codility/src/test/java/counters/CountersTest.java
+++ codility/src/test/java/counters/CountersTest.java
@@ -15,4 +15,20 @@ public class CountersTest {
         int[] E = {3, 2, 2, 4, 2};
         Assertions.assertEquals(Arrays.toString(E), Arrays.toString(counters.solution(X, A)));
     }
+
+    @Test
+    public void getSolutionExample2() {
+        int X = 5;
+        int[] A = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 6, 6, 6, 6, 6, 6};
+        int[] E = {10, 10, 10, 10, 10};
+        Assertions.assertEquals(Arrays.toString(E), Arrays.toString(counters.solution(X, A)));
+    }
+
+    @Test
+    public void getSolutionExample3() {
+        int X = 5;
+        int[] A = {6, 6, 6, 6, 6, 6};
+        int[] E = {0, 0, 0, 0, 0};
+        Assertions.assertEquals(Arrays.toString(E), Arrays.toString(counters.solution(X, A)));
+    }
 }
